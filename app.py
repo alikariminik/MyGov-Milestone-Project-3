54e5785dc2b068370c814ae878b611a236c624fa
alikariminik <alirkariminik@gmail.com>
2023-03-02 20:42:38 +0000

    Added signup view function to app.py

--- app.py
+++ app.py
@@ -36,6 +36,32 @@ def cabinet():
             'There has been an error connecting to the Mongo Database')
 
 
+# Sign Up page (Code from CodeInstitute Lessons)
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("home", username=session["user"]))
+
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
