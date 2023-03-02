8ce3d5f1375a0b3ddd156ddb9f2b9e9be7832e06
alikariminik <alirkariminik@gmail.com>
2023-03-02 22:08:01 +0000

    Added removeFlash function

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,8 @@ window.onload = function () {
   var pathArray = window.location.pathname.split("/");
   var name = pathArray[pathArray.length-1];
 };
+
+function removeFlash() {
+  const element = document.getElementById("flash-message");
+  element.remove();
+};
