6d463c2b4204e9b7cbb090027c5e35fb984617fa
alikariminik <alirkariminik@gmail.com>
2023-03-02 20:44:22 +0000

    Added login  view function to app.py

--- app.py
+++ app.py
@@ -62,6 +62,35 @@ def signup():
     return render_template("signup.html")
 
 
+# Log in page (Code from CodeInstitute Lessons)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Success! You are logged in")
+                        return redirect(url_for(
+                            "home", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
